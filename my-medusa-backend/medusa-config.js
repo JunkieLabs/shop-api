b5bb4734f0db2323d104de1a412d1ef6b8240b13
Niraj Prakash <nirajprakash13@gmail.com>
2023-07-14 15:50:24 +0530

    added meilisearch

--- my-medusa-backend/medusa-config.js
+++ my-medusa-backend/medusa-config.js
@@ -50,6 +50,43 @@ const plugins = [
       autoRebuild: true,
     },
   },
+  // ...
+  {
+    resolve: `medusa-plugin-meilisearch`,
+    options: {
+      // config object passed when creating an instance
+      // of the MeiliSearch client
+      config: {
+        host: process.env.MEILISEARCH_HOST,
+        apiKey: process.env.MEILISEARCH_API_KEY,
+      },
+      settings: {
+        // index settings...
+
+        products: {
+          indexSettings: {
+            searchableAttributes: [
+              "title", 
+              "description",
+              "variant_sku",
+            ],
+            displayedAttributes: [
+              "title", 
+              "description", 
+              "variant_sku", 
+              "thumbnail", 
+              "handle",
+            ],
+          },
+          primaryKey: "id",
+          // transformer: (product) => ({
+          //   id: product.id, 
+          //   // other attributes...
+          // }),
+        },
+      },
+    },
+  }
 ];
 
 const modules = {
